589489a9340ab975474a9ea5af456ee8d37a5903
danodev <daniel.bj.rd@gmail.com>
2023-01-25 21:45:33 -0600

    .

---
+++ src/AppData/Promociones.js
@@ -0,0 +1,33 @@
+export const promociones = [
+  {
+    name: '2x1 esmaltado',
+    price: 400,
+    timeTo: '1.5 hrs'
+  },
+  {
+    name: '2x1 esmaltado',
+    price: 400,
+    timeTo: '1.5 hrs'
+  },
+  {
+    name: '2x1 esmaltado',
+    price: 400,
+    timeTo: '1.5 hrs'
+  },
+  {
+    name: '2x1 esmaltado',
+    price: 400,
+    timeTo: '1.5 hrs'
+  },
+  {
+    name: '2x1 esmaltado',
+    price: 400,
+    timeTo: '1.5 hrs'
+  },
+  {
+    name: '2x1 esmaltado',
+    price: 400,
+    timeTo: '1.5 hrs'
+  },
+  
+]
